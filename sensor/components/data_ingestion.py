0167abdc268248ac4565bd5672adb8f83f224828
Riz1999 <rizwanzhad@gmail.com>
2023-03-28 07:13:48 +0530

    commit

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -1,10 +1,12 @@
+from sensor.data_access.sensor_data import SensorData
 from sensor.exception import SensorException
 from sensor.logger import logging
 from sensor.entity.config_entity import DataIngestionConfig
 from sensor.entity.artifact_entity import DataIngestionArtifact
 #from sklearn.model_selection import train_test_split
 import os,sys
-# from pandas import DataFrame
+from pandas import DataFrame
+
 # from sensor.data_access.sensor_data import SensorData
 # from sensor.utils.main_utils import read_yaml_file
 # from sensor.constant.training_pipeline import SCHEMA_FILE_PATH
@@ -18,24 +20,26 @@ class DataIngestion:
             raise SensorException(e,sys)
 
     def export_data_into_feature_store(self) -> DataFrame:
-    #     """
-    #     Export mongo db collection record as data frame into feature
-    #     """
-    #     try:
-    #         logging.info("Exporting data from mongodb to feature store")
-    #         sensor_data = SensorData()
-    #         dataframe = sensor_data.export_collection_as_dataframe(collection_name=self.data_ingestion_config.collection_name)
-    #         feature_store_file_path = self.data_ingestion_config.feature_store_file_path            
+        pass
+        """
+        Export mongo db collection record as data frame into feature
+        """
+        try:
+            logging.info("Exporting data from mongodb to feature store")
+            sensor_data = SensorData()
+            dataframe = sensor_data.export_collection_as_dataframe(collection_name=self.data_ingestion_config.collection_name)
+            feature_store_file_path = self.data_ingestion_config.feature_store_file_path            
 
-    #         #creating folder
-    #         dir_path = os.path.dirname(feature_store_file_path)
-    #         os.makedirs(dir_path,exist_ok=True)
-    #         dataframe.to_csv(feature_store_file_path,index=False,header=True)
-    #         return dataframe
-    #     except  Exception as e:
-    #         raise  SensorException(e,sys)
+            #creating folder
+            dir_path = os.path.dirname(feature_store_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            dataframe.to_csv(feature_store_file_path,index=False,header=True)
+            return dataframe
+        except  Exception as e:
+            raise  SensorException(e,sys)
 
-    # def split_data_as_train_test(self, dataframe: DataFrame) -> None:
+    def split_data_as_train_test(self, dataframe: DataFrame) -> None:
+        pass
     #     """
     #     Feature store dataset will be split into train and test file
     #     """
@@ -72,12 +76,12 @@ class DataIngestion:
 
     def initiate_data_ingestion(self) -> DataIngestionArtifact:
         try:
-            pass
-            # dataframe = self.export_data_into_feature_store()
+            
+             dataframe = self.export_data_into_feature_store()
             # dataframe = dataframe.drop(self._schema_config["drop_columns"],axis=1)
-            # self.split_data_as_train_test(dataframe=dataframe)
-            # data_ingestion_artifact = DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,
-            # test_file_path=self.data_ingestion_config.testing_file_path)
-            # return data_ingestion_artifact
+             self.split_data_as_train_test(dataframe=dataframe)
+             data_ingestion_artifact = DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,
+             test_file_path=self.data_ingestion_config.testing_file_path)
+             return data_ingestion_artifact
         except Exception as e:
             raise SensorException(e,sys)
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -5,7 +5,7 @@ from sensor.entity.artifact_entity import DataIngestionArtifact
 from sensor.exception import SensorException
 import sys,os
 from sensor.logger import logging
-#from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_ingestion import DataIngestion
 #from sensor.components.data_validation import DataValidation
 ##from sensor.components.model_trainer import ModelTrainer
 #from sensor.components.model_evaluation import ModelEvaluation
@@ -17,21 +17,19 @@ class TrainPipeline:
 #         self.training_pipeline_config = TrainingPipelineConfig()
         
     def __init__(self):
-        training_pipeline_config = TrainingPipelineConfig()
-        self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-        self.training_pipeline_config = TrainingPipelineConfig
+        #training_pipeline_config = TrainingPipelineConfig()
+        #self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        self.training_pipeline_config = TrainingPipelineConfig()
     
     
     def start_data_ingestion(self)->DataIngestionArtifact:
          try: 
+              self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
               logging.info("Starting data ingestion")
-              logging.info("data ingestion completed")
-    #         self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
-    #         logging.info("Starting data ingestion")
-    #         data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
-    #         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-    #         logging.info(f"Data ingestion completed and artifact: {data_ingestion_artifact}")
-    #         return data_ingestion_artifact
+              data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+              data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+              logging.info(f"Data ingestion completed and artifact: {data_ingestion_artifact}")
+              return data_ingestion_artifact
          except  Exception as e:
              raise  SensorException(e,sys)
 
