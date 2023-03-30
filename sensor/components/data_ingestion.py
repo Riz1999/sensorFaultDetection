f4856e06b62547b16062388a93838756aeab08c1
Riz1999 <rizwanzhad@gmail.com>
2023-03-30 23:40:02 +0530

    commit

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -4,7 +4,7 @@ from sensor.exception import SensorException
 from sensor.logger import logging
 from sensor.entity.config_entity import DataIngestionConfig
 from sensor.entity.artifact_entity import DataIngestionArtifact
-#from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 from  pandas import DataFrame
 
 
@@ -40,39 +40,39 @@ class DataIngestion:
             raise  SensorException(e,sys)
 
     def split_data_as_train_test(self, dataframe: DataFrame) -> None:
-        pass
-    #     """
-    #     Feature store dataset will be split into train and test file
-    #     """
+        
+        """
+        Feature store dataset will be split into train and test file
+        """
 
-    #     try:
-    #         train_set, test_set = train_test_split(
-    #             dataframe, test_size=self.data_ingestion_config.train_test_split_ratio
-    #         )
+        try:
+            train_set, test_set = train_test_split(
+                dataframe, test_size=self.data_ingestion_config.train_test_split_ratio
+            )
 
-    #         logging.info("Performed train test split on the dataframe")
+            logging.info("Performed train test split on the dataframe")
 
-    #         logging.info(
-    #             "Exited split_data_as_train_test method of Data_Ingestion class"
-    #         )
+            logging.info(
+                "Exited split_data_as_train_test method of Data_Ingestion class"
+            )
 
-    #         dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
 
-    #         os.makedirs(dir_path, exist_ok=True)
+            os.makedirs(dir_path, exist_ok=True)
 
-    #         logging.info(f"Exporting train and test file path.")
+            logging.info(f"Exporting train and test file path.")
 
-    #         train_set.to_csv(
-    #             self.data_ingestion_config.training_file_path, index=False, header=True
-    #         )
+            train_set.to_csv(
+                self.data_ingestion_config.training_file_path, index=False, header=True
+            )
 
-    #         test_set.to_csv(
-    #             self.data_ingestion_config.testing_file_path, index=False, header=True
-    #         )
+            test_set.to_csv(
+                self.data_ingestion_config.testing_file_path, index=False, header=True
+            )
 
-    #         logging.info(f"Exported train and test file path.")
-    #     except Exception as e:
-    #         raise SensorData(e,sys)
+            logging.info(f"Exported train and test file path.")
+        except Exception as e:
+            raise SensorData(e,sys)
     
 
     def initiate_data_ingestion(self) -> DataIngestionArtifact:
